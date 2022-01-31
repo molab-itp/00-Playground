a88b26995773a72fcb6d2f9ad1c89cd86e638192
John Henry Thompson <jht1493@gmail.com>
2022-01-31 18:31:25 -0500

    dict

--- 01-Javascript.playground/Pages/objects.xcplaygroundpage/Contents.swift
+++ 01-Javascript.playground/Pages/objects.xcplaygroundpage/Contents.swift
@@ -14,9 +14,22 @@
  
  */
 
-let dict:[String : Any] = ["x": 0, "y": 1.0, "color": "red", "list": [1, 2, 3]]
+var dict:[String : Any] = ["x": 0, "y": 1.0, "color": "red", "list": [1, 2, 3]]
 print("dict \(dict)")
 
 // strong/strict type vs. weak/dynamic/free type
+
+var point1 = ["x": 1, "y": 100]
+print("point \(point1)")
+
+point1["x"] = 10
+print("point \(point1)")
+
+// ERROR! type of x inferred to be Int
+// point1["x"] = 9.0
+
+// Ok since dict is type [String : Any]
+dict["x"] = 9.0
+print("dict \(dict)")
 
 //: [Previous](@previous)  [Next](@next)
