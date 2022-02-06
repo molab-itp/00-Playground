eaf674b0b72f93d8ca1f34b6e1025808872d71a2
John Henry Thompson <jht1493@gmail.com>
2022-02-06 12:45:11 -0500

    Update ContentView.swift

--- SpriteExplore/Shared/ContentView.swift
+++ SpriteExplore/Shared/ContentView.swift
@@ -5,19 +5,24 @@
 import SwiftUI
 import SceneKit
 import SpriteKit
+import UIKit
+
+let spriteLen = 80.0
+let frameSize = CGSize(width: 300, height: 600)
+let spriteSize = CGSize(width: frameSize.width/4, height: frameSize.width/4)
 
 struct ContentView: View {
   let scene = Self.buildScene()
   
   var body: some View {
     SpriteView(scene: scene)
-      .frame(width: 300, height: 300)
+      .frame(width: frameSize.width, height: frameSize.height)
       .border(Color.primary)
   }
   
   static func buildScene() -> BouncingSquares {
     let scene = BouncingSquares()
-    scene.size = CGSize(width: 300, height: 300)
+    scene.size = CGSize(width: frameSize.width, height: frameSize.height)
     scene.scaleMode = .fill
     
     scene.populate()
@@ -28,15 +33,15 @@ struct ContentView: View {
 class BouncingSquares: SKScene {
   //  let colors:[NSColor] = [.systemRed, .systemOrange, .systemYellow, .systemGreen, .systemBlue, .systemPurple, .systemPink]
   //  let colors:[UIColor] = [.systemRed, .systemOrange, .systemYellow, .systemGreen, .systemBlue, .systemPurple, .systemPink]
-  let colors:[UIColor] = [.systemRed, .systemYellow, .systemGreen, .systemGray]
+  let colors:[UIColor] = [.systemRed, .systemYellow, .systemGreen, .white]
   var moving = true
   
   func populate() {
     for _ in 0..<4 {
-      let square = SKSpriteNode(color: colors[self.children.count % colors.count], size: CGSize(width: 20, height: 20))
+      let square = SKSpriteNode(color: colors[self.children.count % colors.count], size: spriteSize)
       
-      square.position = CGPoint(x: .random(in: 0...300), y: .random(in: 0...300))
-      square.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 20, height: 20))
+      square.position = CGPoint(x: .random(in: 0...frameSize.width), y: .random(in: 0...frameSize.height))
+      square.physicsBody = SKPhysicsBody(rectangleOf: spriteSize)
       addChild(square)
     }
     
@@ -64,9 +69,7 @@ class BouncingSquares: SKScene {
   //    self.moving.toggle()
   //    configureSquares()
   //  }
-  override func touchesBegan(_ touches: Set<UITouch>,
-                             with event: UIEvent?)
-  {
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     self.moving.toggle()
     configureSquares()
   }
