1ddc71347181d1edaf79eded615654f35faa2941
John Henry Thompson <jht1493@gmail.com>
2022-02-06 13:37:14 -0500

    moving = false

--- SpriteExplore/Shared/ContentView.swift
+++ SpriteExplore/Shared/ContentView.swift
@@ -34,13 +34,19 @@ class BouncingSquares: SKScene {
   //  let colors:[NSColor] = [.systemRed, .systemOrange, .systemYellow, .systemGreen, .systemBlue, .systemPurple, .systemPink]
   //  let colors:[UIColor] = [.systemRed, .systemOrange, .systemYellow, .systemGreen, .systemBlue, .systemPurple, .systemPink]
   let colors:[UIColor] = [.systemRed, .systemYellow, .systemGreen, .white]
-  var moving = true
+  var moving = false
   
   func populate() {
-    for _ in 0..<4 {
+    let n = 3
+    for index in 0..<n {
       let square = SKSpriteNode(color: colors[self.children.count % colors.count], size: spriteSize)
-      
-      square.position = CGPoint(x: .random(in: 0...frameSize.width), y: .random(in: 0...frameSize.height))
+      // let square = SKSpriteNode(imageNamed: "icon-on-gray-1024")
+
+      // square.position = CGPoint(x: .random(in: 0...frameSize.width), y: .random(in: 0...frameSize.height))
+      // let x = CGFloat(index) * spriteSize.width + spriteSize.width/2 + spriteSize.width/2
+      let x = CGFloat(index+1) * spriteSize.width
+      let y = frame.height - spriteSize.width/2
+      square.position = CGPoint(x: x, y: y)
       square.physicsBody = SKPhysicsBody(rectangleOf: spriteSize)
       addChild(square)
     }
@@ -50,7 +56,9 @@ class BouncingSquares: SKScene {
   
   func configureSquares() {
     for square in children {
-      square.physicsBody?.velocity = moving ? CGVector(dx: .random(in: -200...200), dy: .random(in: -200...200)) : .zero
+      let w = frameSize.width
+      let h = frameSize.height
+      square.physicsBody?.velocity = moving ? CGVector(dx: .random(in: -w...w), dy: .random(in: -h...h)) : .zero
       square.physicsBody?.restitution = moving ? 1.0 : 0.0
       square.physicsBody?.linearDamping = moving ? 0.0 : 1.0
       square.physicsBody?.angularDamping = moving ? 0.0 : 1.0
